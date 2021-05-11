75a43d43f13a18f55a6b2ed205a9825469d1b47b
Alon-mizrahi <alonmiz1@gmail.com>
2021-05-11 20:10:42 +0200

    added simple player movement, a player object, enemy object, and game manager

---
+++ KIDS-following-Clone/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public GameObject GMobject;
+    GameManager GM;
+
+    float PlayerSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GM = GMobject.GetComponent<GameManager>();
+        PlayerSpeed = GM.PlayerSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+
+
+    void PlayerMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * PlayerSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.TransformDirection(Vector3.right) * Time.deltaTime * PlayerSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * PlayerSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += transform.TransformDirection(Vector3.back) * Time.deltaTime * PlayerSpeed;
+        }
+    }
+
+
+
+}
